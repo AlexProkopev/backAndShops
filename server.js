5c455c2e5909b15ca5742251fe786f3fe1cf4e89
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2025-01-08 17:17:20 +0100

    change port

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 require("dotenv").config();
 
 require("./db.js");
+const config = require("./config");
+const PORT = process.env.PORT || config.get("serverPort");
 
 const app = require("./app.js");
 
-app.listen(5050, () => console.log("Server running on port 5050..."));
+app.listen(PORT, () => console.log("Server running on port 5050..."));
