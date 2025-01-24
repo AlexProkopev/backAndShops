05f79beddbccca56926218d3d721f5e28d1e6e04
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2025-01-24 11:04:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ const cors = require('cors');
 const app = require("./app.js");
 
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-app.options('*', cors());
+app.use(cors());
 
 const PORT = process.env.PORT || 5050
 
