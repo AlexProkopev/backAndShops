57ae8fdbc0144fe0fb482a26d301b2cf34bcb58f
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2025-01-24 11:16:45 +0100

    update

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const routes = require("./routes");
 const app = express();
+const cors = require('cors');
 
 app.use("/api", routes);
+app.use(
+  cors({
+    origin: "*", // Укажите URL вашего клиента
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Разрешённые методы
+    allowedHeaders: ["Content-Type", "Authorization"], // Разрешённые заголовки
+    credentials: true, // Если передаются cookies
+  })
+);
+
+app.options("*", cors()); // Разрешить все OPTIONS-запросы
+
+
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
@@ -11,6 +24,8 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   const { status, message } = err;
   res.status(status).json({ message });
+  console.log("CORS Middleware triggered for:", req.headers.origin);
+  next()
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -2,17 +2,12 @@ require("dotenv").config();
 require("./db.js");
 
 const config = require("./config");
-const cors = require('cors');
 const app = require("./app.js");
 
 
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
-app.options('*', cors());
+
+
 
 const PORT = process.env.PORT || 5050
 
