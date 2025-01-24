3366c964f5364ad34fa7de5108bd39a43424d66d
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2025-01-24 11:05:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ require("dotenv").config();
 require("./db.js");
 
 const config = require("./config");
-const cors = require('cors');
 const app = require("./app.js");
 
-
-app.use(cors());
 
 const PORT = process.env.PORT || 5050
 
