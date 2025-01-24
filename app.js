04a475ce07deefdf4372f1a6d58aa3b4c1b02f86
AlexProkopev <129211387+AlexProkopev@users.noreply.github.com>
2025-01-24 11:06:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const routes = require("./routes");
+const cors = require('cors');
 const app = express();
 
-app.use("/api", routes);
 
+app.use("/api", routes);
+app.use(cors());
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
 });
+
 
 app.use((err, req, res, next) => {
   const { status, message } = err;
